7a396e8cfe4ed1a03876a4746a0de3faec5335b1
carlos-osorio-developer <osorio.uis@gmail.com>
2022-01-31 19:15:59 -0500

    Add fetch async-functions and display it on website

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -1,9 +1,16 @@
+import 'regenerator-runtime/runtime';
 import fetch from "node-fetch";
 
 const newPalette = async () => {
-  const response = await fetch('http://colormind.io/api/', {method:'POST', headers: {'Content-Type': 'application/json',},body: JSON.stringify({model : "default"})})
+  const response = await fetch('http://colormind.io/api/', {method:'POST',body: JSON.stringify({model : "default"})});
   const data = await response.json();
   return data;
 }
 
-export default newPalette;
+const newApp = async () => {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps', {method:'POST', headers: {'Content-Type': 'application/json'}});
+  const data = await response.json();
+  return data;
+}
+
+export { newPalette, newApp };
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,15 @@
+import {newPalette, newApp} from "./api";
+
 console.log("Hello World!");
+
+const loadData = async () => {
+  const newPal = await newPalette();
+  console.log(newPal);
+  document.getElementById("esparta").innerHTML = newPal.result;
+  const newApp = await newApp();
+  document.body.appendChild(newApp.result);
+};
+
+loadData();
+
+
