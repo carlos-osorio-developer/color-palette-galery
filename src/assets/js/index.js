e5d4b0d803db4dae5823e835c33fb13742e63c37
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-01 16:15:38 -0500

    Add FontAwesome package

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,10 @@
 import { newPalette, userAPI } from './api';
+import '../styles/stylesheet.scss';
+a
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
 
 newPalette();
 userAPI.getLikes();
