6a2f426664f6b70ebd75de71119d730b580ed423
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-01 18:51:44 -0500

    Retrieve colors from API and display them in DIVS

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,30 @@
 import { newPalette, userAPI } from './api';
 import '../styles/stylesheet.scss';
-a
+
 import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
 
-newPalette();
-userAPI.getLikes();
+const loadData = async () => {
+  const main = document.getElementsByTagName('main')[0];
+  const paletteCard = document.createElement('div');
+  paletteCard.classList.add('palette-card');  
+  const paletteContent = document.createElement('div');
+  paletteContent.classList.add('palette-content');  
+  for (let i = 0; i < 6; i++) {      
+    const response = await newPalette();
+    const colors = await response.result;
+    // identifier = colors.flat(1).join('')
+    colors.forEach(element => {
+      const color = document.createElement('div');
+      color.style.cssText = `background-color: rgb(${element.join(',')}); height: 100px; width: 100px;`;
+      paletteContent.appendChild(color);
+    });
+    paletteCard.appendChild(paletteContent);
+    main.appendChild(paletteCard);        
+    // main.innerHTML = `<p>${colors.flat(1).join('')}</p>`;
+  }
+};
+
+loadData();
