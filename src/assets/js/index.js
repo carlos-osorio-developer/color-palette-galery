bf2a96e9dab8792cd6ba25bdb64b200c8d6cb9f3
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-02 07:43:59 -0500

    Add styles and tags for title of palette card

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,7 +7,7 @@ import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
 
 const loadData = async () => {
-  const main = document.getElementsByTagName('main')[0];    
+  const main = document.getElementsByTagName('main')[0];
   for (let i = 0; i < 6; i++) {      
     const paletteCard = document.createElement('div');
     paletteCard.classList.add('palette-card');  
@@ -23,7 +23,20 @@ const loadData = async () => {
       paletteContent.appendChild(color);
     });
     paletteCard.appendChild(paletteContent);
-    main.appendChild(paletteCard);        
+    main.appendChild(paletteCard);
+    const paletteTitle = document.createElement('div');
+    paletteTitle.id = 'palette-title';
+    const paletteName = document.createElement('h2');    
+    paletteName.innerText = `Palette ${i + 1}`;
+    const likes = document.createElement('p');
+    likes.innerText = ': 0';
+    const likesIcon = document.createElement('i');
+    likesIcon.classList.add('fas', 'fa-heart');
+    likes.prepend(likesIcon);
+    paletteTitle.appendChild(paletteName);  
+    paletteTitle.appendChild(likes);
+    paletteCard.appendChild(paletteTitle);
+
     // main.innerHTML = `<p>${colors.flat(1).join('')}</p>`;
   }
 };
