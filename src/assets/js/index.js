807b86c06df94a0e68e85776f435f98c40531576
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-03 16:30:44 -0500

    Add comments section content

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -9,7 +9,7 @@ import '@fortawesome/fontawesome-free/js/brands.js';
 const loadData = async () => {
   const main = document.getElementsByTagName('main')[0];
   for (let i = 0; i < 6; i++) {      
-    const paletteCard = document.createElement('div');
+    const paletteCard = document.createElement('div');    
     paletteCard.classList.add('palette-card'); 
     const paletteHeader = document.createElement('div');
     paletteHeader.classList.add('palette-header');
@@ -18,10 +18,11 @@ const loadData = async () => {
 
     const response = await newPalette();
     const colors = await response.result;
-    // identifier = colors.flat(1).join('')
+    paletteCard.id = `id-${colors.flat(1).join('_')}`;    
     colors.forEach(element => {
       const color = document.createElement('div');
-      color.classList.add('colors');
+      color.id = `id-${element.join('-')}`;
+      color.classList.add('colors');      
       color.style.cssText = `background-color: rgb(${element.join(',')})`;
       paletteColors.appendChild(color);
     });
@@ -44,15 +45,72 @@ const loadData = async () => {
     paletteHeader.appendChild(paletteTitle);
         
     const commentsButton = document.createElement('span');
-    commentsButton.id = 'comments-button';
+    commentsButton.classList.add('comments-button');
     commentsButton.innerText = 'Comments';
     const commentIcon = document.createElement('i');
     commentIcon.classList.add('fas', 'fa-comment');
     commentsButton.prepend(commentIcon);    
-    paletteCard.appendChild(commentsButton);
-
-    // main.innerHTML = `<p>${colors.flat(1).join('')}</p>`;
+    paletteCard.appendChild(commentsButton);  
+    commentsButton.addEventListener('click', function() {
+      commentsModal(this);
+    }
+    );  
   }
+  
+};
+
+const commentsModal = function(element) {  
+  const commentsModal = document.createElement('div');
+  commentsModal.id = 'comments-modal';
+  const title = document.createElement('h2');
+  title.innerText = 'Comment on this palette';
+  const closeIcon = document.createElement('i');
+  closeIcon.classList.add('fas', 'fa-times');         
+
+  const paletteHeader = element.parentElement.children[0].cloneNode(true);  
+  for (let i = 0; i < 5; i++) {    
+    const rgbCode = paletteHeader.children[0].children[i].id.split('-');
+    const hexCode = rgbToHex(rgbCode[1]) + rgbToHex(rgbCode[2]) + rgbToHex(rgbCode[3]);
+    const code = document.createElement('p');
+    code.innerText = `#${hexCode}`;
+    paletteHeader.children[0].children[i].appendChild(code);
+  }
+
+  commentsModal.appendChild(title);
+  commentsModal.appendChild(closeIcon);
+  commentsModal.appendChild(paletteHeader);
+  document.body.children[1].appendChild(commentsModal);
+
+  //add comments section
+  const commentsSection = document.createElement('div');
+  commentsSection.id = 'comments-section';
+  const comments = document.createElement('div');
+  comments.id = 'comments';
+  const commentForm = document.createElement('form');
+  commentForm.id = 'comment-form';
+  const commentInput = document.createElement('input');
+  commentInput.id = 'comment-input';
+  commentInput.type = 'text';
+  commentInput.placeholder = 'Add a comment';
+  const commentButton = document.createElement('button');
+  commentButton.id = 'comment-button';
+  commentButton.type = 'submit';
+  commentButton.innerText = 'Add comment';
+  commentForm.appendChild(commentInput);
+  commentForm.appendChild(commentButton);
+  comments.appendChild(commentForm);
+  commentsSection.appendChild(comments);
+  commentsModal.appendChild(commentsSection);
+
+};
+
+const rgbToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
 };
 
 loadData();
+
